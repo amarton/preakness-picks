08cb52de07738834b420fbe5309c368d7feb4e33
Adam Marton <adam@adammarton.com>
2016-06-23 16:19:09 -0400

    Initial commit
    Initial commit

---
+++ preakness-picks.js
@@ -0,0 +1,156 @@
+
+
+		
+
+		var data = [
+ {
+   "id":1,
+   "race":"Race 1: 10:30 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":2,
+   "race":"Race 2: 11:30 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":3,
+   "race":"Race 3: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":4,
+   "race":"Race 4: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":5,
+   "race":"Race 5: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":6,
+   "race":"Race 6: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":7,
+   "race":"Race 7: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":8,
+   "race":"Race 8: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":9,
+   "race":"Race 9: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":10,
+   "race":"Race 10: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":11,
+   "race":"Race 11: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":12,
+   "race":"Race 12: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":13,
+   "race":"Race 13: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ },
+ {
+   "id":14,
+   "race":"Race 14: 11:15 am",
+   "analysis":"<p>Finnegan ships in from Keeneland, where he finished a tidy second in better company. La Nina and Sunshine Life also ship in from slightly classi- er racing circuits, and thus should improve off recent efforts.",
+   "value":"<p>Sunshine Life finished third in a field of four at Aqueduct, but the class of that maiden race was better than the group she faces Saturday. Hard to say what she’s got, but at 5-1, it’s a decent value in an otherwise even field.",
+   "bet":"<p>Take Finnegan to win. He looks to be the best. Wheel Finnegan with ALL in exacta wagers."
+ }
+];
+
+
+
+		var nextlinkID = 2;
+		var prevlinkID = 1;
+
+
+		function nextRace(raceID){
+
+
+			$.each(data, function(idx, obj) {
+			 	if (obj.id == raceID) {
+				
+					race = obj.race;
+					analysis = obj.analysis;
+					value = obj.value;		
+					bet = obj.bet;					
+				}
+
+			});
+
+		  
+		  $('#race').html(race);
+		  $('#analysis').html(analysis);
+		  $('#value').html(value);
+		  $('#bet').html(bet);
+		   nextlinkID = ++raceID;
+		   prevlinkID = nextlinkID - 2;
+		   $('#chooseDistrictText').html(race);
+
+		   if (prevlinkID !== 0) {
+		
+				$('#prev').show();
+			} else {
+				$('#prev').hide();
+			}
+
+			//SET THIS NUMBER -- 4 -- THE HIGHEST RACE ID PLUS 1
+		   if (nextlinkID == 15) {
+		
+				$('#next').hide();
+			} else {
+				$('#next').show();
+			}
+
+
+
+		};
+
+
+
+
